5d461cddaa5af7f6e02f7175c176fdf980954f64
surajPOC <156636403+surajPOC@users.noreply.github.com>
2024-05-01 13:51:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+const app = express();
+const port = 8000;
+
+// Health check route
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "up",
+    timestamp: new Date().toISOString(),
+  });
+});
+
+app.get("/getPolicyDetails", async (req, res) => {
+  res.status(200).json({
+    successful: true,
+    output_data: {
+      attributes: {
+        premium: "2500",
+      },
+    },
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
